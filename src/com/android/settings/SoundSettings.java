b3ae59d526da04143c9eed525b9e6412fca13b79
kasperhettinga <kasper.hettinga@gmail.com>
2013-11-16 00:43:43 +0100

    Return to the old-fashioned device settings option
    And move headset connect option to sound settings
    
    Change-Id: I862bce2aa406b9153dba243abca71421e0f08929

--- src/com/android/settings/SoundSettings.java
+++ src/com/android/settings/SoundSettings.java
@@ -74,6 +74,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private static final String KEY_RINGTONE = "ringtone";
     private static final String KEY_NOTIFICATION_SOUND = "notification_sound";
     private static final String KEY_CATEGORY_CALLS = "category_calls_and_notification";
+    private static final String KEY_HEADSET_CONNECT_PLAYER = "headset_connect_player";
     private static final String KEY_DOCK_CATEGORY = "dock_category";
     private static final String KEY_DOCK_AUDIO_SETTINGS = "dock_audio";
     private static final String KEY_DOCK_SOUNDS = "dock_sounds";
@@ -102,6 +103,8 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private Runnable mRingtoneLookupRunnable;
 
     private AudioManager mAudioManager;
+
+    private CheckBoxPreference mHeadsetConnectPlayer;
 
     private Preference mDockAudioSettings;
     private CheckBoxPreference mDockSounds;
@@ -242,6 +245,10 @@ public class SoundSettings extends SettingsPreferenceFragment implements
             }
         };
 
+        mHeadsetConnectPlayer = (CheckBoxPreference) findPreference(KEY_HEADSET_CONNECT_PLAYER);
+        mHeadsetConnectPlayer.setChecked(Settings.System.getInt(resolver,
+                Settings.System.HEADSET_CONNECT_PLAYER, 0) != 0);
+
         initDockSettings();
     }
 
@@ -362,6 +369,9 @@ public class SoundSettings extends SettingsPreferenceFragment implements
                     super.onPreferenceTreeClick(ps, ps);
                 }
             }
+        } else if (preference == mHeadsetConnectPlayer) {
+            Settings.System.putInt(getContentResolver(), Settings.System.HEADSET_CONNECT_PLAYER,
+                    mHeadsetConnectPlayer.isChecked() ? 1 : 0);
         } else if (preference == mDockSounds) {
             Settings.Global.putInt(getContentResolver(), Settings.Global.DOCK_SOUNDS_ENABLED,
                     mDockSounds.isChecked() ? 1 : 0);
